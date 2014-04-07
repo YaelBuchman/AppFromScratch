24972667723351acf151191aabced45631698c23
YaelBuchman <y.bochman@applicaster.com>
2014-04-07 13:23:37 +0300

    AppFromScratch project is created

---
+++ AppFromScratch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppFromScratch
+//
+//  Created by Yael Buchman on 4/7/14.
+//  Copyright (c) 2014 ___APPLICASTER___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppFromScratchTests/AppFromScratchTests.m
@@ -0,0 +1,34 @@
+//
+//  AppFromScratchTests.m
+//  AppFromScratchTests
+//
+//  Created by Yael Buchman on 4/7/14.
+//  Copyright (c) 2014 ___APPLICASTER___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppFromScratchTests : XCTestCase
+
+@end
+
+@implementation AppFromScratchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
